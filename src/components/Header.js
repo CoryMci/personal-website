f2751eaeaa0b4602c402b1114253a113bab33f0a
CoryMci <corymciver@live.ca>
2023-03-04 14:35:04 -0800

    Improve responsiveness, add pomo project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 export default function Header() {
   return (
-    <ul className="flex justify-end header uppercase text-lg tracking-widest">
-      <li className="m-4 px-8 pb-8 hover:border-b-4 hover:mb-1 border-red-500">
+    <ul className="flex justify-center md:justify-end header uppercase text-lg tracking-widest">
+      <li className="m-2 sm:m-4 md:px-8 pb-8 hover:border-b-4 hover:mb-1 border-red-500">
         <a href="#home">Home</a>
       </li>
-      <li className="m-4 px-8 pb-8 hover:border-b-4 hover:mb-1 border-red-500">
+      <li className="m-2 sm:m-4 md:px-8 pb-8 hover:border-b-4 hover:mb-1 border-red-500">
         <a href="#about">About</a>
       </li>
-      <li className="m-4 px-8 pb-8 hover:border-b-4 hover:mb-1 border-red-500">
+      <li className="m-2 sm:m-4 md:px-8 pb-8 hover:border-b-4 hover:mb-1 border-red-500">
         <a href="#projects">Projects</a>
       </li>
     </ul>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,13 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export default function Welcome() {
-  const titles = [
-    "Accountant",
-    "Programmer",
-    "Climber",
-    "Surfer",
-    "Backpacker",
-  ];
+  const titles = ["Accountant", "Programmer", "Climber", "Backpacker"];
   const [title, setTitle] = useState(titles[0]);
 
   function nextTitle() {
@@ -31,11 +25,13 @@ export default function Welcome() {
   }, [title]);
 
   return (
-    <div className="min-h-[90vh] flex flex-col justify-center indent-5">
-      <div className="text-6xl uppercase tracking-wider">Cory McIver</div>
-      <div className="text-3xl tracking-wide">
+    <div className="min-h-[90vh] flex flex-col justify-center pl-4">
+      <div className="text-4xl md:text-6xl sm:text-3xl sm:text-bold uppercase tracking-wider">
+        Cory McIver
+      </div>
+      <div className="flex text-2xl flex-col md:flex-row md:text-3xl sm:text-xl sm:text-bold tracking-wide">
         <span className="line-through pr-2">Web Developer</span>
-        {title}
+        <span>{title}</span>
       </div>
     </div>
   );
